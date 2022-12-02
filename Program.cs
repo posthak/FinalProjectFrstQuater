d89a5497ae91d75f1f33b759834a24628e6a3176
posthak <adler_m@mail.ru>
2022-12-03 02:01:10 +0600

    Project added

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.Clear();
+String[] array = { "hello", "2", "world", ":-)" };
+String[] arrayResult = new String[GetNewArrayLength(array)];
+
+PrintArrayResult(array, arrayResult);
+
+int GetNewArrayLength(String[] arr)
+{
+    int j = 0;
+    foreach (var it in arr)
+    {
+        if (it.Length <= 3)
+        {
+            j++;
+        }
+    }
+    return j;
+}
+
+void PrintArrayResult(String[] ar, String[] arRes)
+{
+    int i = 0;
+    foreach (var item in ar)
+    {
+        if (item.Length <= 3)
+        {
+            arRes[i] = item;
+            i++;
+        }
+    }
+    Console.WriteLine($"[\"{String.Join("\", \"", ar)}\"] -> [\"{String.Join("\", \"", arRes)}\"]");
+}
